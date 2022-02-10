9bc4df6e1afe38dd0dd23407f18bf9daf8e3213e
datboiluiskrrt <datboiluiskrrt@gmail.com>
2022-02-10 15:47:52 -0600

    Added routes

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 require("dotenv").config();
+console.log(process.env.PORT, process.env.NODE_ENV);
+const cors = require("cors");
 const express = require("express");
 
 const server = express();
+
+server.use(express.json());
+server.use(cors());
 
 server.get("/api/users", (req, res) => {
   res.json([
@@ -9,6 +14,17 @@ server.get("/api/users", (req, res) => {
     { id: 2, username: "faa" },
     { id: 3, username: "fee" },
   ]);
+});
+
+server.use("*", (req, res, next) => {
+  res.send(`<h1>Hello, there!</h1>`);
+});
+
+server.use((error, req, res, next) => {
+  res.status(500).son({
+    message: error.message,
+    stack: error.stack,
+  });
 });
 
 const PORT = process.env.PORT || 8080;
